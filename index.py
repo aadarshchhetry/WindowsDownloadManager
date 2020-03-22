afcfadb5d8585fbb149762bb970ce9166429cc24
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-23 02:39:56 +0530

    connected MainApp to ui+download button

--- index.py
+++ index.py
@@ -7,6 +7,32 @@ class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainApp, self).__init__()
         uic.loadUi('main.ui', self)
+        self.InitUI()
+        self.Handle_Buttons()
+    
+    def InitUI(self):
+        ##contain all ui changes in loading
+        pass
+    
+    def Handle_Buttons(self):
+        ##handle all buttons in the app
+        self.pushButton.clicked.connect(self.Download)
+
+    def handle_Progress(self):
+        ##calculate the progress
+        pass
+
+    def handle_Browse(self):
+        ##enable bwosing ro our os, browse, save location
+        pass
+
+    def Download(self):
+        ##downloading any files
+        print("Starting Download...")
+    
+    def Save_Browse(self):
+        ##save location
+        pass
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
